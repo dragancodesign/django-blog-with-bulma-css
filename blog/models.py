0deccee35151739dc539de1e7b235a1ea971206d
Dragan CodeSign <dragan.codesign@gmail.com>
2022-05-22 19:47:26 +0300

    python manage.py migrate DONE

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE) # When you delete post it will delete all of it's comments, on_delete->is action
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)

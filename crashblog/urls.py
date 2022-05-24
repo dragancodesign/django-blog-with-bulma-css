371ee27572344d5ac4d80558b97b2a0e47dd75dd
Dragan CodeSign <dragan.codesign@gmail.com>
2022-05-24 12:52:08 +0300

    Sitemap related stuff corrected, now everything works

--- crashblog/urls.py
+++ crashblog/urls.py
@@ -15,19 +15,19 @@ Including another URLconf
 """
 from django.conf import settings # added to handle images
 from django.conf.urls.static import static # added to handle images
-# from django.contrib import sitemaps
-# from django.contrib.sitemaps.views import sitemap
+from django.contrib import sitemaps
+from django.contrib.sitemaps.views import sitemap
 from django.contrib import admin
 from django.urls import path, include
 
-# from .sitemaps import CategorySitemap, PostSitemap
+from .sitemaps import CategorySitemap, PostSitemap
 
 from core.views import frontpage, about, robots_txt
 
-# sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
+sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
 
 urlpatterns = [
-    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('robots.txt', robots_txt, name='robots_txt'),
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
